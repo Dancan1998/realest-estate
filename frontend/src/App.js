c4c5d952c1db69aa8abdea419996f0089b20f6c7
Dancan Chibole <dancankingstar@gmail.com>
2022-04-03 00:57:20 +0300

    React setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,35 @@
 import "./App.css";
+import Layout from "./hocs/layout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  Home,
+  About,
+  Contact,
+  Listings,
+  SignIn,
+  SignUp,
+  ListingDetail,
+  NotFound,
+} from "./pages";
+import "./sass/main.scss";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/listings" element={<Listings />} />
+          <Route exact path="/listing/:id" element={<ListingDetail />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/login" element={<SignIn />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import ListingDetail from "./ListingDetail";
+import Listings from "./Listings";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import NotFound from "./NotFound";
+
+export {
+  Home,
+  About,
+  Contact,
+  ListingDetail,
+  Listings,
+  SignIn,
+  SignUp,
+  NotFound,
+};
